a7601169248215517032e7ffc96bf4fa63bfaf97
DanalaBhavya <dhanalabhavya1555@gmail.com>
2024-07-03 18:51:31 +0530

    4

--- Basic py proj/rps.py
+++ Basic py proj/rps.py
@@ -1,34 +1,34 @@
 import random
 choices=["rock","paper","scissors"]
+computer=random.choice(choices)
 player=False
 cscore=0
 pscore=0
 while True:
-    computer=random.choice(choices)
     player=input("rock,paper,scissors?").capitalize()
     if player==computer:
         print("tie")
     elif player=="rock":
-        if computer=="scissors":
-            print("player wins")
-            pscore+=1
-        else:
-            print("player lose")
-            cscore+=1
-    elif player=="paper":
-        if computer=="rock":
-            print("player wins")
-            pscore+=1
-        else:
-            print("player lose")
-            cscore+=1
-    elif player=="scissors":
         if computer=="paper":
-            print("player wins")
-            pscore+=1
-        else:
             print("player lose")
             cscore+=1
+        else:
+            print("player win")
+            pscore+=1
+    elif player=="paper":
+        if computer=="scissors":
+            print("player lose")
+            cscore+=1
+        else:
+            print("player win")
+            pscore+=1
+    elif player=="scissors":
+        if computer=="rock":
+            print("player lose")
+            cscore+=1
+        else:
+            print("player win")
+            pscore+=1
     elif player=="End":
         print("final scores:")
         print("Player score:",pscore)
