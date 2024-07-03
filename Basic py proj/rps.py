e57126a8b8f5cac5486713a8fbd3a2956b063fcb
DanalaBhavya <dhanalabhavya1555@gmail.com>
2024-07-03 18:53:15 +0530

    6

--- Basic py proj/rps.py
+++ Basic py proj/rps.py
@@ -1,37 +1,37 @@
 import random
-choices=["rock","paper","scissors"]
-computer=random.choice(choices)
-player=False
-cscore=0
-pscore=0
+choices = ["Rock", "Paper", "Scissors"]
+computer = random.choice(choices)
+player = False
+cpu_score = 0
+player_score = 0
 while True:
-    player=input("rock,paper,scissors?").capitalize()
-    if player==computer:
-        print("tie")
-    elif player=="rock":
-        if computer=="paper":
-            print("player lose")
-            cscore+=1
+    player = input("Rock, Paper or  Scissors?").capitalize()
+    if player == computer:
+        print("Tie!")
+    elif player == "Rock":
+        if computer == "Paper":
+            print("You lose!", computer, "covers", player)
+            cpu_score+=1
         else:
-            print("player win")
-            pscore+=1
-    elif player=="paper":
-        if computer=="scissors":
-            print("player lose")
-            cscore+=1
+            print("You win!", player, "smashes", computer)
+            player_score+=1
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer, "cut", player)
+            cpu_score+=1
         else:
-            print("player win")
-            pscore+=1
-    elif player=="scissors":
-        if computer=="rock":
-            print("player lose")
-            cscore+=1
+            print("You win!", player, "covers", computer)
+            player_score+=1
+    elif player == "Scissors":
+        if computer == "Rock":
+            print("You lose...", computer, "smashes", player)
+            cpu_score+=1
         else:
-            print("player win")
-            pscore+=1
-    elif player=="End":
-        print("final scores:")
-        print("Player score:",pscore)
-        print("computer score:",cscore)
-        break        
+            print("You win!", player, "cut", computer)
+            player_score+=1
+    elif player=='End':
+        print("Final Scores:")
+        print(f"CPU:{cpu_score}")
+        print(f"Plaer:{player_score}")
+        break
                 
