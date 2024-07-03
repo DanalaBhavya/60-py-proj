82a5d2c15f843168df076a3af4a6c9130b3d4448
DanalaBhavya <dhanalabhavya1555@gmail.com>
2024-07-03 19:21:34 +0530

    7

---
+++ Basic py proj/colored.py
@@ -0,0 +1,6 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.RED+Back.YELLOW+"I am Bhavya")
+print(Fore.BLUE + Back.GREEN +"I am from CBIT"+Back.CYAN+"HYDERABAD")
