478eb7fb2bac35a9a673fa211600eb3863793a01
DanalaBhavya <dhanalabhavya1555@gmail.com>
2024-07-06 16:37:23 +0530

    first

--- Basic py proj/alarm.py
+++ Basic py proj/alarm.py
@@ -1,22 +1,21 @@
-from datetime import datetime
+from datetime import datetime   
 from playsound import playsound
-alarm_time=input("Enter the time:HH:MM:SS PM\n")
-alarm_hr=alarm_time[0:2]
-alarm_min=alarm_time[3:5]
-alarm_sec=alarm_time[6:8]
-alarm_period=alarm_time[9:11].upper()
-print('setting up alarm')
+alarm_time = input("Enter the time of alarm to be set:HH:MM:SS\n")
+alarm_hour=alarm_time[0:2]
+alarm_minute=alarm_time[3:5]
+alarm_seconds=alarm_time[6:8]
+alarm_period = alarm_time[9:11].upper()
+print("Setting up alarm..")
 while True:
-    now=datetime.now()
-    current_hr=now.strftime("%I")
-    current_min=now.strftime("%M")
-    current_sec=now.strftime("%S")
-    current_period=now.strftime("%p")
+    now = datetime.now()
+    current_hour = now.strftime("%I")
+    current_minute = now.strftime("%M")
+    current_seconds = now.strftime("%S")
+    current_period = now.strftime("%p")
     if(alarm_period==current_period):
-        if(alarm_hr==current_hr):
-            if(alarm_min==current_min):
-                if(alarm_sec==current_sec):
+        if(alarm_hour==current_hour):
+            if(alarm_minute==current_minute):
+                if(alarm_seconds==current_seconds):
                     print("Wake Up!")
                     playsound('heatwaves.mp3')
                     break
-    
